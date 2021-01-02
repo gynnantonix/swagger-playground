60b091d92d5700bd00af5d4c20957098baf98ab1
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-01-02 17:42:38 -0500

    slightly less-generated code

--- restapi/configure_todo_list.go
+++ restapi/configure_todo_list.go
@@ -4,21 +4,106 @@ package restapi
 
 import (
 	"crypto/tls"
+	"fmt"
 	"net/http"
+	"sync"
+	"sync/atomic"
 
-	"github.com/go-openapi/errors"
-	"github.com/go-openapi/runtime"
-	"github.com/go-openapi/runtime/middleware"
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	middleware "github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/swag"
 
+	"github.com/gynnantonix/swagger-playground/models"
 	"github.com/gynnantonix/swagger-playground/restapi/operations"
 	"github.com/gynnantonix/swagger-playground/restapi/operations/todos"
 )
 
-//go:generate swagger generate server --target ../../swagger-playground --name TodoList --spec ../swagger.yml --principal interface{}
+var exampleFlags = struct {
+	Example1 string `long:"example1" description:"Sample for showing how to configure cmd-line flags"`
+	Example2 string `long:"example2" description:"Further info at https://github.com/jessevdk/go-flags"`
+}{}
 
 func configureFlags(api *operations.TodoListAPI) {
-	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+	api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{
+		swag.CommandLineOptionsGroup{
+			ShortDescription: "Example Flags",
+			LongDescription:  "",
+			Options:          &exampleFlags,
+		},
+	}
 }
+
+// simple storage
+var items = make(map[int64]*models.Item)
+var lastID int64
+
+var itemsLock = &sync.Mutex{}
+
+func newItemID() int64 {
+	return atomic.AddInt64(&lastID, 1)
+}
+
+func addItem(item *models.Item) error {
+	if item == nil {
+		return errors.New(500, "missing item")
+	}
+
+	itemsLock.Lock()
+	defer itemsLock.Unlock()
+
+	newID := newItemID()
+	item.ID = newID
+	items[newID] = item
+
+	return nil
+}
+
+func updateItem(id int64, item *models.Item) error {
+	if item == nil {
+		return errors.New(500, "item must be present")
+	}
+
+	itemsLock.Lock()
+	defer itemsLock.Unlock()
+
+	_, exists := items[id]
+	if !exists {
+		return errors.NotFound("not found: item %d", id)
+	}
+
+	item.ID = id
+	items[id] = item
+	return nil
+}
+
+func deleteItem(id int64) error {
+	itemsLock.Lock()
+	defer itemsLock.Unlock()
+
+	_, exists := items[id]
+	if !exists {
+		return errors.NotFound("not found: item %d", id)
+	}
+
+	delete(items, id)
+	return nil
+}
+
+func allItems(since int64, limit int32) (result []*models.Item) {
+	result = make([]*models.Item, 0)
+	for id, item := range items {
+		if len(result) >= int(limit) {
+			return
+		}
+		if since == 0 || id > since {
+			result = append(result, item)
+		}
+	}
+	return
+}
+
+//go:generate swagger generate server --target ../../swagger-playground --name TodoList --spec ../swagger.yml --principal interface{}
 
 func configureAPI(api *operations.TodoListAPI) http.Handler {
 	// configure the api here
@@ -32,34 +117,41 @@ func configureAPI(api *operations.TodoListAPI) http.Handler {
 
 	api.UseSwaggerUI()
 	// To continue using redoc as your UI, uncomment the following line
-	// api.UseRedoc()
+	api.UseRedoc()
 
 	api.JSONConsumer = runtime.JSONConsumer()
 
 	api.JSONProducer = runtime.JSONProducer()
 
-	if api.TodosAddOneHandler == nil {
-		api.TodosAddOneHandler = todos.AddOneHandlerFunc(func(params todos.AddOneParams) middleware.Responder {
-			return middleware.NotImplemented("operation todos.AddOne has not yet been implemented")
-		})
-	}
-	if api.TodosDestroyOneHandler == nil {
-		api.TodosDestroyOneHandler = todos.DestroyOneHandlerFunc(func(params todos.DestroyOneParams) middleware.Responder {
-			return middleware.NotImplemented("operation todos.DestroyOne has not yet been implemented")
-		})
-	}
-	if api.TodosFindTodosHandler == nil {
-		api.TodosFindTodosHandler = todos.FindTodosHandlerFunc(func(params todos.FindTodosParams) middleware.Responder {
-			return middleware.NotImplemented("operation todos.FindTodos has not yet been implemented")
-		})
-	}
-	if api.TodosUpdateOneHandler == nil {
-		api.TodosUpdateOneHandler = todos.UpdateOneHandlerFunc(func(params todos.UpdateOneParams) middleware.Responder {
-			return middleware.NotImplemented("operation todos.UpdateOne has not yet been implemented")
-		})
-	}
-
-	api.PreServerShutdown = func() {}
+	api.TodosAddOneHandler = todos.AddOneHandlerFunc(func(params todos.AddOneParams) middleware.Responder {
+		if err := addItem(params.Body); err != nil {
+			return todos.NewAddOneDefault(500).WithPayload(&models.Error{Code: 500, Message: swag.String(err.Error())})
+		}
+		return todos.NewAddOneCreated().WithPayload(params.Body)
+	})
+	api.TodosDestroyOneHandler = todos.DestroyOneHandlerFunc(func(params todos.DestroyOneParams) middleware.Responder {
+		if err := deleteItem(params.ID); err != nil {
+			return todos.NewDestroyOneDefault(500).WithPayload(&models.Error{Code: 500, Message: swag.String(err.Error())})
+		}
+		return todos.NewDestroyOneNoContent()
+	})
+	api.TodosFindTodosHandler = todos.FindTodosHandlerFunc(func(params todos.FindTodosParams) middleware.Responder {
+		mergedParams := todos.NewFindTodosParams()
+		mergedParams.Since = swag.Int64(0)
+		if params.Since != nil {
+			mergedParams.Since = params.Since
+		}
+		if params.Limit != nil {
+			mergedParams.Limit = params.Limit
+		}
+		return todos.NewFindTodosOK().WithPayload(allItems(*mergedParams.Since, *mergedParams.Limit))
+	})
+	api.TodosUpdateOneHandler = todos.UpdateOneHandlerFunc(func(params todos.UpdateOneParams) middleware.Responder {
+		if err := updateItem(params.ID, params.Body); err != nil {
+			return todos.NewUpdateOneDefault(500).WithPayload(&models.Error{Code: 500, Message: swag.String(err.Error())})
+		}
+		return todos.NewUpdateOneOK().WithPayload(params.Body)
+	})
 
 	api.ServerShutdown = func() {}
 
@@ -74,18 +166,21 @@ func configureTLS(tlsConfig *tls.Config) {
 // As soon as server is initialized but not run yet, this function will be called.
 // If you need to modify a config, store server instance to stop it individually later, this is the place.
 // This function can be called multiple times, depending on the number of serving schemes.
-// scheme value will be set accordingly: "http", "https" or "unix"
+// scheme value will be set accordingly: "http", "https" or "unix".
 func configureServer(s *http.Server, scheme, addr string) {
+	if exampleFlags.Example1 != "something" {
+		fmt.Print("example1 argument is not something")
+	}
 }
 
 // The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
-// The middleware executes after routing but before authentication, binding and validation
+// The middleware executes after routing but before authentication, binding and validation.
 func setupMiddlewares(handler http.Handler) http.Handler {
 	return handler
 }
 
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
-// So this is a good place to plug in a panic handling middleware, logging and metrics
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
 	return handler
 }
